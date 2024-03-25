01f64f8c5fa67dc1d6d38dfc417bab5139e2ffe8
Sumasribhogi2003 <93893785+Sumasribhogi2003@users.noreply.github.com>
2024-03-25 15:31:11 +0530

    home page updated

--- src/Components/Header/Dropdown.js
+++ src/Components/Header/Dropdown.js
@@ -1,44 +1,158 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import MenuIcon from '@mui/icons-material/Menu';
-import HomeIcon from "@mui/icons-material/Home";
-import DraftsIcon from "@mui/icons-material/Drafts";
-import ApartmentIcon from "@mui/icons-material/Apartment";
 import "./Dropdown.css";
+import { Link, useNavigate } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import GroupsIcon from "@mui/icons-material/Groups";
+//import SchoolIcon from "@mui/icons-material/School";
+// import PersonIcon from "@mui/icons-material/Person";
+import DraftsIcon from "@mui/icons-material/Drafts";
+//import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
+import ApartmentIcon from "@mui/icons-material/Apartment";
+import DescriptionIcon from "@mui/icons-material/Description";
+import MenuIcon from "@mui/icons-material/Menu";
+import CampaignIcon from '@mui/icons-material/Campaign';
+
+
+import { useState } from "react";
+// import { PiExamFill } from "react-icons/pi";
 
 const Dropdown = () => {
+  const navigate = useNavigate();
+
+
+
+  
   const [menuState, setMenuState] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuState(!menuState);
+  const homeHandler = () => {
+    navigate("/");
   };
-
+  // eslint-disable-next-line no-lone-blocks
+  {/*const contactHandler = () => {
+    navigate("/contact-us");
+  };*/}
   return (
     <nav className="topnav">
-      <div className="menu-icon" onClick={toggleMenu}>
+      <div className="menu-icon" onClick={() => setMenuState(!menuState)}>
         <MenuIcon />
-        <p>MENU</p>
+        MENU
       </div>
       <div className={menuState ? "menu-on-options" : "menu-off-options"}>
-        <div className="dropdown" onClick={toggleMenu}>
-          <Link to="/" className="dropbtn">
-            <HomeIcon />
+        <div className="dropdown">
+          <div className="dropbtn" onClick={homeHandler}>
+            <div className="drop-icon" style={{ verticalAlign: 'middle' }}>
+              <HomeIcon />
+            </div>
             HOME
-          </Link>
+          </div>
         </div>
-        <div className="dropdown" onClick={toggleMenu}>
-          <Link to="/dmc" className="dropbtn">
-            <ApartmentIcon />
-            DMC
-          </Link>
+
+
+
+        <div class="dropdown">
+          <div class="dropbtn">
+            <ApartmentIcon /> &nbsp;
+            ABOUT
+          </div>
+          <div class="dropdown-content" onClick={() => setMenuState(!menuState)}>
+            <button >
+              {" "}
+              <Link className="link-btn" to="/dmc/about-dmc">
+                About DMC Cell
+              </Link>
+            </button>
+            <button>
+              {" "}
+              <Link className="link-btn" to="/dmc/dmc-co-ordinator">
+                DMC Co-Ordinator
+              </Link>
+            </button>
+            <button>
+              {" "}
+              <Link className="link-btn" to="/dmc/dmc-activities">
+                DMC Activities
+              </Link>
+            </button>
+            <button>
+              {" "}
+              <Link className="link-btn" to="/dmc/dmc-events">
+              DMC Events
+              </Link>
+            </button>
+            <button>
+              {" "}
+              <Link className="link-btn" to="/dmc/dmc-future-plans">
+              DMC Future Plans
+              </Link>
+            </button>
+            <button>
+              {" "}
+              <Link className="link-btn" to="/dmc/dmc-facilities">
+                DMC Facilities
+              </Link>
+            </button>
+          </div>
         </div>
-        <div className="dropdown" onClick={toggleMenu}>
-          <Link to="/e-ticket" className="dropbtn">
+
+        <div className="dropdown">
+          <div className="dropbtn">
+            <GroupsIcon />
+            <Link to="/about-elearn" className="link-btn">
+              E-LEARN
+            </Link>
+          </div>
+        </div>
+
+        <div className="dropdown">
+          <div className="dropbtn">
+            <CampaignIcon />
+            <Link to="/about-surveillance" className="link-btn">
+              SURVEILLANCE
+            </Link>
+          </div>
+        </div>
+
+        <div className="dropdown">
+          <div className="dropbtn">
+            <DescriptionIcon />
+            <Link to="/about-gallery" className="link-btn">
+              GALLERY
+            </Link>
+          </div>
+        </div>
+
+        <div className="dropdown">
+          <div className="dropbtn">
+            <DescriptionIcon />
+            <Link to="/about-eticket" className="link-btn">
+              E-TICKET
+            </Link>
+          </div>
+        </div>
+
+        <div className="dropdown">
+          <div className="dropbtn">
+            <DescriptionIcon />
+            <Link to="/about-dmc-team" className="link-btn">
+              DMC TEAM
+            </Link>
+          </div>
+        </div>
+
+       
+
+      <div className="dropdown">
+          <div className="dropbtn">
             <DraftsIcon />
-            E-Ticket
-          </Link>
+            <Link to="/contact-us" className="link-btn">
+              CONTACT US
+            </Link>
+          </div>
         </div>
-        {/* Add more dropdowns for other menu items */}
+
+
+
+
+
       </div>
     </nav>
   );
