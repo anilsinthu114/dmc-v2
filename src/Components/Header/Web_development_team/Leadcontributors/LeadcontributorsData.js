58cc964f057a8b4892208934d8abd12e6fc31d06
SINTHU ANIL <155183088+AnilSinthu73@users.noreply.github.com>
2024-11-30 15:47:10 +0530

    added 2024 batch

--- src/Components/Header/Web_development_team/Leadcontributors/LeadcontributorsData.js
+++ src/Components/Header/Web_development_team/Leadcontributors/LeadcontributorsData.js
@@ -1,32 +1,31 @@
-// TeamData.js
-
 const teamData = [
-    {
-      name: 'Haneesh Bandaru',
-      rollNumber: '20VV1A1220',
-      year: '2023-2024',
-    },
-    
-    {
-        name: 'Thiruvayapeta Srimanth Kumar',
-        rollNumber: '20VV1A1259',
-        year: '2023-2024',
-    },
-    
-     
-      {
-        name: '	Dannana Sai Ajith Kumar',
-        rollNumber: '21VV5A1268',
-        year: '2023-2024',
-      },
-     
-    
-     
-      
+  {
+    name: 'Haneesh Bandaru',
+    rollNumber: '20VV1A1220',
+    year: '2023-2024',
+  },
+  {
+    name: 'Thiruvayapeta Srimanth Kumar',
+    rollNumber: '20VV1A1259',
+    year: '2023-2024',
+  },
+  {
+    name: 'Dannana Sai Ajith Kumar',
+    rollNumber: '21VV5A1268',
+    year: '2023-2024',
+  },
+  {
+    name: 'Anil Sinthu',
+    rollNumber: '22VV5A1273',
+    year: '2024-2025',
+    role: 'Full Stack Developer',
+  },
+  {
+    name: 'Harshavivek Gangadahar',
+    rollNumber: '22VV5A1274', // Example roll number, update as needed
+    year: '2024-2025',
+    role: 'Frontend Developer',
+  },
+];
 
-
-
-  ];
-  
-  export default teamData;
-  
+export default teamData;
